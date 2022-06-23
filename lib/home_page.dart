eed5453336f761b32df0c171fcb18f25f806d0fa
Keval Gadhiya <65587277+keval-gadhiya@users.noreply.github.com>
2022-06-23 12:04:05 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const int days = 30;
+    const String name = "Keval Gadhiya";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: const Center(
+        child: Text("Welcom to $days days of Flutter by $name"),
+      ),
+    );
+  }
+}
