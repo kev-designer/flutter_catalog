7650d5c7c24050bd0f57a439fd95ad4d2d5f1be0
Keval Gadhiya <65587277+keval-gadhiya@users.noreply.github.com>
2022-06-26 12:11:39 +0530

    Day 15 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,9 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_catalog/models/catalog.dart';
-import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
