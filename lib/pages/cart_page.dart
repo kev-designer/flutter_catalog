28668aeb31b8be72dc03ef27b61f71bc43a76857
Keval Gadhiya <65587277+keval-gadhiya@users.noreply.github.com>
2022-06-29 20:06:11 +0530

    Day 22 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -15,6 +15,75 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       backgroundColor: context.canvasColor,
+      body: Column(
+        children: [
+          const _CartList().p32().expand(),
+
+          //DIVIDER
+          Divider(
+            color: context.theme.accentColor,
+            thickness: 1.0,
+          ),
+
+          //TOTAL VALUE CART
+          const _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+//CARD TOTAL
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.bold.xl4.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                context.theme.buttonColor,
+              ),
+              shape: MaterialStateProperty.all(
+                const StadiumBorder(),
+              ),
+            ),
+            onPressed: () {},
+            child: "Buy".text.xl2.semiBold.make(),
+          ).w32(context).h8(context),
+        ],
+      ),
+    );
+  }
+}
+
+//CART LIST
+
+class _CartList extends StatelessWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const Icon(Icons.done),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.remove_circle_outline),
+          ),
+          title: "Item".text.make(),
+        );
+      },
     );
   }
 }
